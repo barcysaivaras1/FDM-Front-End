85fce30b96ea4901d961a4463fb2910f06bb67aa
Mohamed Ait-Hocine <ec22479@qmul.ac.uk>
2024-03-24 17:27:19 +0000

    Minor fix to vite.config and minor tweaks to Login on mobile

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 const fullReloadAlways = {
   name: 'full-reload-always',
