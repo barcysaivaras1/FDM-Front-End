994b5fa4bcf42bd02fbb3d407709a891126d57a2
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-26 19:44:24 +0000

    Fix backend request

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,31 @@ import axios from 'axios'
 
 
 function App() {
+  /* IMPORTANT - this is only a basic showcase and requires you to manually insert a session cookie into your browser.
+  */
+
+  // fetches a user's profile
   const fetch =  () => {
     axios.get(
-      `http://127.0.0.1:5000/users/profile`, {withCredentials:true}
+      // use localhost rather than 127.0.0.1 for consistency
+      // withCredentials allows a cookie to be passed alongside the request which is important to authenticate the user
+      `http://localhost:5000/users/profile`, { withCredentials:true }
     )
-    .then(function(response){console.log(response)})
-    console.log("giigiti")
+    .then(function(response){
+      console.log(response)
+    })
   }
+
   useEffect(() => {
     fetch()
   }, [])
 
   return (
-    <Login />
+    // <Login />
+    <div>
+      {/* the default pfp file is being hosted on the server at path {backend_folder}/static/default.png */}
+      <img src="http://localhost:5000/static/default.png" width={100} />
+    </div>
   )
 }
 
