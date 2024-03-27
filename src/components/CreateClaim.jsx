145ea1dfc8ac6305cf60329489052985ece5b673
Konrad Dominik Vincler <k.d.vincler@se22.qmul.ac.uk>
2024-03-27 22:43:37 +0000

    Implemented image uploading when creating a claim

--- src/components/CreateClaim.jsx
+++ src/components/CreateClaim.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import '../css/CreateClaim.css'
+import { CiImageOn } from "react-icons/ci";
 
 function CreateClaim () {
-    const [title, setTitle] = useState(null);
-    const [type, setType] = useState(null);
-    const [currency, setCurrency] = useState(null);
-    const [amount, setAmount] = useState(null);
-    const [description, setDescription] = useState(null);
+    const [title, setTitle] = useState();
+    const [type, setType] = useState();
+    const [currency, setCurrency] = useState();
+    const [amount, setAmount] = useState();
+    const [description, setDescription] = useState();
+    const [image, setImage] = useState(null);
+    const [preview, setPreview] = useState();
 
     function handeSubmit (e) {
         e.preventDefault();
@@ -15,7 +18,8 @@ function CreateClaim () {
 
         // for now it will just alarm the user with the
         // inputted data fo8r debugging puproses
-        alert(`Successful submit. \nTitle: ${title} \nType: ${type} \nCurrency: ${currency} \nAmount: ${amount} \nDescription: ${description}`);
+        alert(`Successful submit. \nTitle: ${title} \nType: ${type} \nCurrency: ${currency} \nAmount: ${amount} \nDescription: ${description} \nImage: ${image} \nPreview: ${preview}`);
+        console.log(image)
     }
 
     document.title = "Create new claim"
@@ -23,25 +27,84 @@ function CreateClaim () {
         <div className='createContainer'>
             <legend className='createLegend'>Create Claim</legend>
             <form className='createForm' onSubmit={handeSubmit}>
-                <input type="text" className='infield titleInput' placeholder='Title...' value={title} onChange={(e) => {setTitle(e.target.value)}}/>
-                <select name="expenseType" className='infield typeInput' value={type} onChange={(e) => {setType(e.target.value)}}>
-                    <option value="placeholder" disabled selected>Type of Expense</option>
+                <input type="text" 
+                    className='infield titleInput' 
+                    name='title'
+                    placeholder='Title...' 
+                    value={title} 
+                    onChange={(e) => {setTitle(e.target.value)}}
+                    required
+                />
+                <select name="expenseType" 
+                    className='infield typeInput' 
+                    value={type} 
+                    onChange={(e) => {setType(e.target.value)}}
+                    required
+                >
+                    <option value="" disabled selected hidden>Type of Expense</option>
                     <option value="Travel">Travel Expense</option>
                     <option value="Accomomdation">Accomomdation Expense</option>
                     <option value="Catering">Catering Expense</option>
                 </select>
                 <div className='inlineContainer'>
-                    <select name="currencyType" className='infield currencyInput' value={currency} onChange={(e) => {setCurrency(e.target.value)}}>
-                        <option value="placeholder" disabled selected>Currency</option>
+                    <select name="currencyType" 
+                        className='infield currencyInput' 
+                        value={currency} 
+                        onChange={(e) => {setCurrency(e.target.value)}}
+                        required
+                    >
+                        <option value="" disabled selected hidden>Currency</option>
                         <option value="GBP">£</option>
                         <option value="USD">$</option>
                         <option value="EUR">€</option>
                     </select>
-                    <input type="number" className='infield amountInput' placeholder='Amount' value={amount} onChange={(e) => {setAmount(e.target.value)}}/>
+                    <input type="number" 
+                        className='infield amountInput' 
+                        name='amount'
+                        placeholder='Amount' 
+                        value={amount} 
+                        onChange={(e) => {setAmount(e.target.value)}}
+                        required
+                    />
                 </div>
-                <textarea name="description" className='infield descriptionInput' placeholder='Description' value={description} onChange={(e) => {setDescription(e.target.value)}}></textarea>
-                {/* TODO: SOMEHOW ADD UPLOAD PICTURE */}
-                <button className='createSubmit'>Submit Claim</button>
+                <textarea name="description" 
+                    className='infield descriptionInput' 
+                    placeholder='Description' 
+                    value={description} 
+                    onChange={(e) => {setDescription(e.target.value)}}
+                    required
+                ></textarea>
+                <div className="proofArea">
+                    {image ? (
+                        <>
+                            <img src={preview} alt="uploaded proof" className='proofPreview'/>
+                            <button className='cancelButton'
+                                onClick={() => {
+                                    setImage(null)
+                                    setPreview()
+                            }}>
+                                Remove
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <CiImageOn />
+                            <input className='imageInput' 
+                                id='file' 
+                                name='file' 
+                                type='file' 
+                                accept='image/*'
+                                onChange={(e) => {
+                                    setImage(e.target.files[0])
+                                    setPreview(URL.createObjectURL(e.target.files[0]))
+                                }}
+                                required
+                            />
+                            <label htmlFor="file" className='almostButton'>Upload an Image</label>
+                        </>
+                    )}
+                </div>
+                <button className='infield createSubmit'>Submit Claim</button>
             </form>
         </div>
     )
