ab759851e8a625726b2f8b348170eb25f765f452
barcysaivaras1 <120397399+barcysaivaras1@users.noreply.github.com>
2024-03-27 13:42:34 +0000

    Routing for login to View
    Setup routing in app and added a route from login to Claimant expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import Login from './components/Login'
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Expense from './components/ClaimantExpenses';
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Login/>}/>
+        <Route path='/expenses' exact element={<Expense/>}/>
+      </Routes>
+    </Router>
   )
 }
 
