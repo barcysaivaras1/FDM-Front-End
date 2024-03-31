955a9763e0b465283ac948db60264191fbf73036
Konrad Dominik Vincler <k.d.vincler@se22.qmul.ac.uk>
2024-03-31 14:40:04 +0100

    Incorporated navbar into CreateClaim and fixed export issues of CreateClaim

--- src/components/CreateClaim.jsx
+++ src/components/CreateClaim.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
+import NavBar from "./navbar";
 import '../css/CreateClaim.css'
 import { CiImageOn } from "react-icons/ci";
 
-function CreateClaim () {
+export function CreateClaim () {
     const [title, setTitle] = useState();
     const [type, setType] = useState();
     const [currency, setCurrency] = useState();
@@ -25,97 +26,100 @@ function CreateClaim () {
 
     document.title = "Create new claim"
     return(
-        <div className='createContainer'>
-            <legend className='createLegend'>Create Claim</legend>
-            <form className='createForm' onSubmit={handeSubmit}>
-                <input type="text" 
-                    className='infield titleInput' 
-                    name='title'
-                    placeholder='Title...' 
-                    value={title} 
-                    onChange={(e) => {setTitle(e.target.value)}}
-                    required
-                />
-                <select name="expenseType" 
-                    className='infield typeInput' 
-                    value={type} 
-                    onChange={(e) => {setType(e.target.value)}}
-                    required
-                >
-                    <option value="" disabled selected hidden>Type of Expense</option>
-                    <option value="Travel">Travel Expense</option>
-                    <option value="Accomomdation">Accomomdation Expense</option>
-                    <option value="Catering">Catering Expense</option>
-                </select>
-                <div className='inlineContainer'>
-                    <select name="currencyType" 
-                        className='infield currencyInput' 
-                        value={currency} 
-                        onChange={(e) => {setCurrency(e.target.value)}}
-                        required
-                    >
-                        <option value="" disabled selected hidden>Currency</option>
-                        <option value="GBP">£</option>
-                        <option value="USD">$</option>
-                        <option value="EUR">€</option>
-                    </select>
-                    <input type="number" 
-                        className='infield amountInput' 
-                        name='amount'
-                        placeholder='Amount' 
-                        value={amount} 
-                        onChange={(e) => {setAmount(e.target.value)}}
+        <div>
+            <NavBar/> 
+            <div className='createContainer'>
+                <legend className='createLegend'>Create Claim</legend>
+                <form className='createForm' onSubmit={handeSubmit}>
+                    <input type="text" 
+                        className='infield titleInput' 
+                        name='title'
+                        placeholder='Title...' 
+                        value={title} 
+                        onChange={(e) => {setTitle(e.target.value)}}
                         required
                     />
-                </div>
-                <input type="text"
-                    onFocus={(e) => {e.target.type = "date"}}
-                    className='infield dateInput'
-                    name="date"
-                    placeholder='Date of expense'
-                    value={date}
-                    onChange={(e) => {setDate(e.target.value)}}
-                    required
-                />
-                <textarea name="description" 
-                    className='infield descriptionInput' 
-                    placeholder='Description' 
-                    value={description} 
-                    onChange={(e) => {setDescription(e.target.value)}}
-                    required
-                ></textarea>
-                <div className="proofArea">
-                    {image ? (
-                        <>
-                            <img src={preview} alt="uploaded proof" className='proofPreview'/>
-                            <button className='cancelButton'
-                                onClick={() => {
-                                    setImage(null)
-                                    setPreview()
-                            }}>
-                                Remove
-                            </button>
-                        </>
-                    ) : (
-                        <>
-                            <CiImageOn />
-                            <input className='imageInput' 
-                                id='file' 
-                                name='file' 
-                                type='file' 
-                                accept='image/*'
-                                onChange={(e) => {
-                                    setImage(e.target.files[0])
-                                    setPreview(URL.createObjectURL(e.target.files[0]))
-                                }}
-                                required
-                            />
-                            <label htmlFor="file" className='almostButton'>Upload an Image</label>
-                        </>
-                    )}
-                </div>
-                <button className='infield createSubmit'>Submit Claim</button>
-            </form>
+                    <select name="expenseType" 
+                        className='infield typeInput' 
+                        value={type} 
+                        onChange={(e) => {setType(e.target.value)}}
+                        required
+                    >
+                        <option value="" disabled selected hidden>Type of Expense</option>
+                        <option value="Travel">Travel Expense</option>
+                        <option value="Accomomdation">Accomomdation Expense</option>
+                        <option value="Catering">Catering Expense</option>
+                    </select>
+                    <div className='inlineContainer'>
+                        <select name="currencyType" 
+                            className='infield currencyInput' 
+                            value={currency} 
+                            onChange={(e) => {setCurrency(e.target.value)}}
+                            required
+                        >
+                            <option value="" disabled selected hidden>Currency</option>
+                            <option value="GBP">£</option>
+                            <option value="USD">$</option>
+                            <option value="EUR">€</option>
+                        </select>
+                        <input type="number" 
+                            className='infield amountInput' 
+                            name='amount'
+                            placeholder='Amount' 
+                            value={amount} 
+                            onChange={(e) => {setAmount(e.target.value)}}
+                            required
+                        />
+                    </div>
+                    <input type="text"
+                        onFocus={(e) => {e.target.type = "date"}}
+                        className='infield dateInput'
+                        name="date"
+                        placeholder='Date of expense'
+                        value={date}
+                        onChange={(e) => {setDate(e.target.value)}}
+                        required
+                    />
+                    <textarea name="description" 
+                        className='infield descriptionInput' 
+                        placeholder='Description' 
+                        value={description} 
+                        onChange={(e) => {setDescription(e.target.value)}}
+                        required
+                    ></textarea>
+                    <div className="proofArea">
+                        {image ? (
+                            <>
+                                <img src={preview} alt="uploaded proof" className='proofPreview'/>
+                                <button className='cancelButton'
+                                    onClick={() => {
+                                        setImage(null)
+                                        setPreview()
+                                }}>
+                                    Remove
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <CiImageOn />
+                                <input className='imageInput' 
+                                    id='file' 
+                                    name='file' 
+                                    type='file' 
+                                    accept='image/*'
+                                    onChange={(e) => {
+                                        setImage(e.target.files[0])
+                                        setPreview(URL.createObjectURL(e.target.files[0]))
+                                    }}
+                                    required
+                                />
+                                <label htmlFor="file" className='almostButton'>Upload an Image</label>
+                            </>
+                        )}
+                    </div>
+                    <button className='infield createSubmit'>Submit Claim</button>
+                </form>
+            </div>
         </div>
     )
 }
