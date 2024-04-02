01aa5e4f5462af82d3eee581cd639d0956aff7ae
Ayotunde Williams Kamardeen Ogunnaiya <a.ogunnaiya@se22.qmul.ac.uk>
2024-04-02 14:59:23 +0100

    implemented Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,1 +1,37 @@
 import '../css/Profile.css'
+import React from 'react'
+import pfp from '../assets/johnpork.png';
+
+
+export const Profile = () => {
+
+    return(
+        
+        <div className="container">
+            <div className='header'>
+                <p className="head">View Profile</p>
+            </div>
+            <div className="personal">
+                <img className='pfp' src={pfp} alt="pfp" />
+                <p className="name">John Pork</p>     
+            </div>
+            <div className="role">
+                Level 1 Employee
+            </div>
+            <div className="data">
+                <p className="title">Line Manager</p>
+                <p className="cont">Jonesy Rothschild</p>
+                <p className="title">Total Accepted Claims</p>
+                <p className="cont">1</p>
+                <p className="title">Total Budget Spent</p>
+                <p className="cont">£49.99</p>
+            </div>
+        </div>
+        
+
+    )
+}
+
+
+
+
