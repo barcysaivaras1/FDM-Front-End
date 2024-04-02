fec0d4b852a8c3edd636a6c3cd1a863c1afd5f67
Konrad Dominik Vincler <k.d.vincler@se22.qmul.ac.uk>
2024-04-02 17:37:30 +0100

    Profile.jsx backend integration (calculators weren't fully tested)

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,6 +8,9 @@ import { getApiURL } from './api';
 
 export function Profile() {
     const [profile, setProfile] = useState()
+    const [totalAccepted, setTotalAccepted] = useState(0);
+    const [totalSpent, setTotalSpent] = useState(0.0);
+
     function fetchProfile() {
         axios.get(
             // getApiURL('/users/profile'), 
@@ -17,7 +20,7 @@ export function Profile() {
             }
         )
         .then(function(response) {
-            console.log(response.data)
+            // console.log(response.data)
             setProfile(response.data)
         })
         .catch(function(error) {
@@ -25,9 +28,29 @@ export function Profile() {
         })
     }
 
+    function calcVals() {
+        // console.log(profile);
+        if (profile.claims.length !== 0) {
+            profile.claims.map((claim) => {
+                if (claim.status == "approved") {
+                    setTotalAccepted(totalAccepted + 1);
+                    setTotalSpent((totalSpent) => {return totalSpent + parseFloat(claim.amount)});
+                    // console.log(claim.amount);
+                }
+            })
+        }
+    }
+
     useEffect(() => {
         fetchProfile();
     }, [])
+
+    useEffect(() => {
+        if (!profile){
+            return;
+        }
+        calcVals();
+    }, [profile])
   
     return(
         
@@ -37,22 +60,69 @@ export function Profile() {
                 <p className="head">View Profile</p>
             </div>
             <div className="personal">
-                <img className='pfp' src={pfp} alt="pfp" />
-                <p className="name">John Pork</p>     
+                {profile ? (
+                    <img className='pfp' src={'http://127.0.0.1:5000/' + profile.profile_picture} alt="pfp" />
+                ) : null}
+                <p className="name">
+                    {profile ? (profile.first_name + " " + profile.last_name) : "-"}
+                </p>     
             </div>
             <div className="role">
-                Level 1 Employee
+                {profile ? (profile.role) : "-"}
             </div>
             <div className="data">
                 <p className="title">Line Manager</p>
-                <p className="cont">Jonesy Rothschild</p>
+                <p className="cont">
+                    {profile ? (profile.line_manager) : "-"}
+                </p>
                 <p className="title">Total Accepted Claims</p>
-                <p className="cont">1</p>
+                <p className="cont">
+                    {/* 1  */}
+                    {(totalAccepted !== 0) ? (totalAccepted) : "0"}
+                </p>
                 <p className="title">Total Budget Spent</p>
-                <p className="cont">£49.99</p>
+                <p className="cont">
+                    {/* £49.99 */}
+                    {(totalSpent !== 0.0) ? ("£" + (totalSpent / 2).toFixed(2) ) : "£0"}
+                </p>
             </div>
         </div>
         
 
     )
 }
+
+
+/*
+    Model reply from server for /users/profile:
+
+    {
+    "claims": [
+        {
+            "amount": "19.99",
+            "description": "I need this for my mental health.",
+            "status": "Pending",
+            "title": "Fortnite card"
+        },
+        {
+            "amount": "420.69",
+            "description": "Please reinburse me this has made me broke",
+            "status": "Pending",
+            "title": "Gucci flip-flops"
+        },
+        {
+            "amount": "21.59",
+            "description": "example description",
+            "status": "Pending",
+            "title": "example title"
+        }
+    ],
+    "first_name": "Armin",
+    "last_name": "Shahnami",
+    "line_manager": "manager",
+    "profile_picture": "/static/profile-pictures/default.png",
+    "role": "Employee",
+    "username": "armin2"
+    }
+
+*/
