48cc6a95015db69e93d205cea39503a0cd2f7b05
Sefa Yildirim <ec221028@qmul.ac.uk>
2024-04-04 11:28:07 +0100

    Updated Line Manager Expenses

--- src/components/LineManagerExpenses.jsx
+++ src/components/LineManagerExpenses.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../css/LineManagerExpenses.css"
 import NavBar from "./NavBar";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export function LineManagerExpenses() {
     const [expenses, setExpenses] = useState([]);
@@ -15,63 +16,158 @@ export function LineManagerExpenses() {
             console.log(error);
         })
 
-    // test datas
-    const exampleExpense = { date_time: "2024/2/21 - 1:10PM", currency_type: "£", amount: "19", desc: "Fortnite Card", state: "Accepted" }
-    const exampleExpense2 = { date_time: "2024/2/21 - 2:20PM", currency_type: "$", amount: "3499.99", desc: "Apple Vision Pro", state: "Declined" }
-    const exampleExpense3 = { date_time: "2024/2/21 - 3:30PM", currency_type: "Robux", amount: "399", desc: "Cool Sunglasses", state: "Pending" }
+    const updateDisplayType = (value) => {
+        setDisplayType({ ...displayType, state:value});
+    };
 
-    const exampleExpenseList = [exampleExpense, exampleExpense2, exampleExpense3]
+    const [displayType, setDisplayType] = useState({state:"All Expenses"});
+
+    // test datas
+    const exampleExpense = {type:"Mental Wellbeing", date_time:"2024/2/21 - 1:10PM",currency_type:"£",amount:"19",title:"Fortnite Card",state:"Accepted"}
+    const exampleExpense2 = {type:"Productivity",date_time:"2024/2/21 - 2:20PM",currency_type:"$",amount:"3499.99",title:"Apple Vision Pro",state:"Denied"}
+    const exampleExpense3 = {type:"Gamer",date_time:"2024/2/21 - 3:30PM",currency_type:"Robux",amount:"399",title:"Cool Sunglasses",state:"Pending"}
     const bigList = [];
     for (let i = 0; i < 100; i++) {
-        bigList.push(exampleExpense);
+        bigList.push(exampleExpense, exampleExpense2, exampleExpense3);
     }
 
-    document.title = "Review expenses";
-
     return (
-        <div className='LMEContainer'>
+        <div className = 'LMEContainer'>
             <NavBar />
-
             <div className='LMEBody'>
-                <h1 id='Title'>Expenses</h1>
+                <h1 className = 'title'>Expenses</h1>
 
-                //options at top of page
-                <div className='optionsList'>
-                    <p className="option">To Review</p>
-                    <p className="option">All Expenses</p>
+                <ToggleButton displayType = {displayType} setDisplayType = {setDisplayType} updateDisplayType = {updateDisplayType}/>
+
+                <div className = "columnTitles">
+                    <p>Date</p> <p>Expense</p> <p>Amount</p> <p>Type</p> <p>State</p>
                 </div>
 
-                //column titles
-                <div className="columnTitles">
-                    <p>Date</p> <p>Expense</p> <p>Amount</p> <p>Type</p>
-                </div>
-
-                //expenses listed vertically in rows
-                <div className="scrollboxExpenses"><ExpenseList eel={bigList} /></div>
+                <div className = "scrollboxExpenses"><ExpenseList listOfClaims = {bigList} displayType = {displayType}/></div>
             </div>
         </div>
     )
 }
 
-const Expense = (props) => {
-    return (
-        <div className='expense'>
-            <div className='claimInfo'>
-                <div className='claimDate'>{props.expense.date_time}</div>
-                <div className='moneyInfo'>{props.expense.currency_type + props.expense.amount}</div>
-                <div className='description'>{props.expense.desc}</div>
-            </div>
-        </div>
-    )
-}
-
-const ExpenseList = (props) => {
-    return Array.from(
-        { length: props.eel.length },
-        (_, i) => (
-            <div className='expensesList'>
-                <Expense expense={props.eel[i]} />
+const ToggleButton = (props) =>{
+    console.log(props.displayType.state)
+    if (props.displayType.state == "All Expenses"){
+        return(
+            <div className = 'optionsList'>
+                <button onClick = {() => props.updateDisplayType("To Review")} className="option">To Review</button>
+                <button onClick = {() => props.updateDisplayType("All Expenses")} className="optionActive">All Expenses</button>
+                <button onClick = {() => props.updateDisplayType("Denied")} className="option">Denied Claims</button>  
+                <button onClick = {() => props.updateDisplayType("Accepted")} className="option">Accepted</button>
             </div>
         )
-    );
+    }
+    if (props.displayType.state == "To Review"){
+        return(
+            <div className = 'optionsList'>
+                <button onClick = {() => props.updateDisplayType("To Review")} className="optionActive">To Review</button>
+                <button onClick = {() => props.updateDisplayType("All Expenses")} className="option">All Expenses</button>
+                <button onClick = {() => props.updateDisplayType("Denied")} className="option">Denied Claims</button>
+                <button onClick = {() => props.updateDisplayType("Accepted")} className="option">Accepted</button>
+            </div>
+        )
+    }
+    if (props.displayType.state == "Denied"){
+        return(
+            <div className = 'optionsList'>
+                <button onClick = {() => props.updateDisplayType("To Review")} className="option">To Review</button>
+                <button onClick = {() => props.updateDisplayType("All Expenses")} className="option">All Expenses</button>
+                <button onClick = {() => props.updateDisplayType("Denied")} className="optionActive">Denied Claims</button>  
+                <button onClick = {() => props.updateDisplayType("Accepted")} className="option">Accepted</button>
+            </div>
+        )
+    }
+    if (props.displayType.state == "Accepted"){
+        return(
+            <div className = 'optionsList'>
+                <button onClick = {() => props.updateDisplayType("To Review")} className="option">To Review</button>
+                <button onClick = {() => props.updateDisplayType("All Expenses")} className="option">All Expenses</button>
+                <button onClick = {() => props.updateDisplayType("Denied")} className="option">Denied Claims</button>  
+                <button onClick = {() => props.updateDisplayType("Accepted")} className="optionActive">Accepted</button>
+            </div>
+        )
+    }
+}
+
+const Expense = (props) =>{
+    return(
+        <ul className = 'claimInfo'>
+            <li>{props.expense.date_time}</li>
+            <li>{props.expense.title}</li>
+            <li>{props.expense.currency_type+props.expense.amount}</li>
+            <li>{props.expense.type}</li>
+            <li>{props.expense.state}</li>
+        </ul>
+    )
+}
+
+const ExpenseList = (props) =>{
+    if (props.displayType.state == "All Expenses") {
+        return Array.from(
+            { length: props.listOfClaims.length },
+            (_, i) => (
+                <Link to = "/">
+                    <Expense expense = {props.listOfClaims[i]} />
+                </Link>
+            )
+        );
+    }
+
+    if (props.displayType.state == "To Review"){
+        let filteredList = [];
+        props.listOfClaims.map(element=>{
+            if(element.state == "Pending" ){
+              filteredList.push(element);
+            }
+        })
+
+        return Array.from(
+            { length: filteredList.length },
+            (_, i) => (
+                <Link>
+                    <Expense expense = {filteredList[i]} />
+                </Link>
+            )
+        );
+    }
+
+    if (props.displayType.state == "Denied"){
+        let filteredList = [];
+        props.listOfClaims.map(element=>{
+            if(element.state == "Denied" ){
+              filteredList.push(element);
+            }
+        })
+
+        return Array.from(
+            { length: filteredList.length },
+            (_, i) => (
+                <Link to ="/">
+                    <Expense expense = {filteredList[i]} />
+                </Link>
+            )
+        );
+    }
+
+    if (props.displayType.state == "Accepted"){
+        let filteredList = [];
+        props.listOfClaims.map(element=>{
+            if(element.state == "Accepted" ){
+              filteredList.push(element);
+            }
+        })
+
+        return Array.from(
+            { length: filteredList.length },
+            (_, i) => (
+                <Link to ="/">
+                    <Expense expense = {filteredList[i]} />
+                </Link>
+            )
+        );
+    }
 }
